9559cfb47418b5075628f194920514a2cc9aaac3
jont21 <jontml96@gmail.com>
2019-08-31 00:47:45 -0600

    Tarea 7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Task from './components/task';
+import Title from './components/title';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTask: "",
+            tasks: []
+        }
+    }
 
+    handleTaskChange = (event) => {
+        this.setState({
+            newTask: event.target.value,
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let newTasks = this.state.tasks;
+        newTasks.push(this.state.newTask)
+        this.setState({
+            newTask: "",
+            tasks: newTasks
+        })
+    }
+    render() {
+        return (
+            <div className = "container" >
+            <Title/>
+            <form onSubmit = { this.handleSubmit }>
+            <input value = { this.state.newTask }
+            onChange = { this.handleTaskChange }
+            type = "text"
+            className = "new-task"/>
+            </form> 
+             { this.state.newTask.map(Task => <Task/>)} 
+             </div>
+        );
+    }
+}
 export default App;
---
+++ src/components/task.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Task() {
+    return ( <div className = "task-container">
+
+        <h3 className = "task" > { "task" } </h3>
+
+        </div>
+
+    );
+}
+export default Task;
